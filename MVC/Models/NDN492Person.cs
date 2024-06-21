6c0c47e2c598584abe03a78ec33979c13e64cc30
nhat2509 <ducnhat756@gmail.com>
2024-06-21 15:19:03 +0700

    NDN492

---
+++ MVC/Models/NDN492Person.cs
@@ -0,0 +1,16 @@
+namespace MVC.Models
+{
+    public class NDN492Person
+    {
+        
+    
+        public string? Malop { get; set; }
+
+        public int? Nhom { get; set; }
+
+        public double SoTT { get; set; }
+      
+        
+
+    }
+}
